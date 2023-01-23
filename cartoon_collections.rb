a7312bb512633ca433b28d6c692255af8aef3bb1
Yuferril <yuferril.yunos@gmail.com>
2023-01-23 23:34:37 +1100

    commit tree f57e6d694753a9234de663acf71911589e50c963

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names)# code an argument here
   # Your code here
+  names.each.with_index do |name, index|
+    puts "#{index +1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.map {|calls| calls.capitalize + "!"} 
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.any? {|call| call.length > 4 }
+
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(find_cheeses)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  find_cheeses.find do |maybe_cheese|
+    cheese_types.include?(maybe_cheese)
+  end
 end
